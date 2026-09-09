cfe370bdc1a5c570131c20d9f4085d030f8af654
pramod <gopinath.pramod@gmail.com>
2026-09-09 12:37:12 +0530

    Fix media and static URL paths to serve tester signatures reliably

--- ndt_demo/settings.py
+++ ndt_demo/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
@@ -131,7 +131,7 @@ STORAGES = {
 }
 
 # Media files
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Authentication settings
